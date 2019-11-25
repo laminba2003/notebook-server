376c04cd58474a722069dc4a1da152eddad9a912
mamadou lamine ba <laminba2003@gmail.com>
2019-11-25 19:37:01 +0100

    updating the PythonScriptEngine class

--- src/main/java/com/oracle/notebook/PythonScriptEngine.java
+++ src/main/java/com/oracle/notebook/PythonScriptEngine.java
@@ -2,6 +2,8 @@ package com.oracle.notebook;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class PythonScriptEngine extends ScriptEngine {
@@ -9,19 +11,20 @@ public class PythonScriptEngine extends ScriptEngine {
 	@Override
 	public Evaluation eval(File file) throws Exception {
 		Process process = new ProcessBuilder("python", file.getAbsolutePath()).start();
-		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-		String output = stdInput.readLine();
-		if (output == null) {
-			StringBuffer buffer = new StringBuffer();
-			String line;
-			while ((line = stdError.readLine()) != null) {
-				buffer.append(line + " ");
-			}
-			output = buffer.toString();
-		}
+		String output = readAllLines(process.getInputStream());
+		output = output!=null ? output : readAllLines(process.getErrorStream()); 
 		Evaluation evaluation = new Evaluation(output);
 		return evaluation;
+	}
+	
+	protected String readAllLines(InputStream stream) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		StringBuffer buffer = new StringBuffer();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			buffer.append(line + " ");
+		}
+		return buffer.toString();
 	}
 
 }
