c778f0463cdc4deadde55c797f3f83e3ccba8407
mamadou lamine ba <laminba2003@gmail.com>
2019-11-26 16:20:21 +0100

    updating the PythonScriptEngine class

--- src/main/java/com/oracle/notebook/PythonScriptEngine.java
+++ src/main/java/com/oracle/notebook/PythonScriptEngine.java
@@ -5,15 +5,22 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 public class PythonScriptEngine extends ScriptEngine {
 
 	@Override
 	public Evaluation eval(File file) throws Exception {
 		Process process = new ProcessBuilder("python", file.getAbsolutePath()).start();
-		String error = getLastLine(process.getErrorStream());
-		return error.equals("") ? new Evaluation(getLastLine(process.getInputStream()), false)
-				: new Evaluation(error, true);
+		process.waitFor(5, TimeUnit.SECONDS);
+		process.destroy();
+		if (process.exitValue() == 0) {
+			String error = getLastLine(process.getErrorStream());
+			return error.equals("") ? new Evaluation(getLastLine(process.getInputStream()), false)
+					: new Evaluation(error, true);
+		} else {
+			return new Evaluation("process aborted", true);
+		}
 	}
 
 	protected String getLastLine(InputStream stream) throws IOException {
